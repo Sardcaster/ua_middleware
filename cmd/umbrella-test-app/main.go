4a37b53c4cbb08a626c41b07124194fafce713d8
Artyom Smirnov <artyiom2013@yandex.ru>
2024-06-23 02:01:30 +0300

    done

--- cmd/umbrella-test-app/main.go
+++ cmd/umbrella-test-app/main.go
@@ -1,57 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"time"
-	"github.com/labstack/echo/v4"
+	"github.com/Sardcaster/ua_middleware/internal/pkg/app"
 )
 
 func main() {
-
-	s := echo.New()
-
-	s.Use(mw)
-
-	s.GET("/status", Handler)
-	err := s.Start(":8080")
+	a, err := app.New()
 	if err!= nil {
         log.Fatal(err)
     }
 	
-
-}
-
-func Handler(ctx echo.Context) error {
-
-	d := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	duration := time.Until(d)
-
-	s := fmt.Sprintf("Days remain: %d", int64(duration.Hours()/24))
-
-	err := ctx.String(http.StatusOK, s)
+	err = a.Run()
 	if err!= nil {
-		return err
-    }
-
-	return nil
-}
-
-func mw(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(ctx echo.Context) error {
-		val := ctx.Request().Header.Get("User-Role")
-		
-		if val == "admin" {
-			log.Println("red button user detected")
-		}
-
-		err := next(ctx)
-		if err!= nil {
-            return err
-        }
-				
-		return nil
+		log.Fatal(err)
 	}
+
 }
